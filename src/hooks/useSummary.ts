7dc804fefc061408ea1dacf5d0d9fa8f615101b1
asammarco <sammarco.andre@gmail.com>
2024-10-01 16:34:44 -0300

    First Commit

---
+++ src/hooks/useSummary.ts
@@ -0,0 +1,33 @@
+import { useMemo } from 'react'
+import { TransactionContext } from '../contexts/TransactionContexts'
+import { useContextSelector } from 'use-context-selector'
+
+export function useSummary() {
+  const transactions = useContextSelector(TransactionContext, (context) => {
+    return context.transactions
+  })
+
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price
+        }
+        if (transaction.type === 'outcome') {
+          acc.outcome += transaction.price
+        }
+
+        acc.total = acc.income - acc.outcome
+
+        return acc
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
+
+  return summary
+}
